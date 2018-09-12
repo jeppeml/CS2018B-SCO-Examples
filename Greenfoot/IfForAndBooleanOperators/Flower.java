309105d8ab3a1d7c05fbf745ede52757b0bfd4e1
jeppeml <jeppeml@users.noreply.github.com>
2018-09-12 12:06:14 +0200

    .

--- Greenfoot/IfForAndBooleanOperators/Flower.java
+++ Greenfoot/IfForAndBooleanOperators/Flower.java
@@ -2,19 +2,36 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Flower extends Actor
 {
+    // Attribute/ Instance variable (only once)
+    boolean doit = true;
+    
+    // Methods / behavior
     public void act()
     {
-        move(1);
+        move(4);
         int i = 0;
         boolean a = true;
         boolean b = true;
-        String name = "jeppe";
-        if(name=="jeppe") // Condition must be true/false
+        
+        
+        // true  && true  == true
+        // false && true  == false
+        // true  && false == false
+        // false && false == false
+        // true  || true  == true
+        // false || true  == true
+        // true  || false == true
+        // false || false == false
+        char[] someChar = {'n','o'};
+        String newOne = new String(someChar); // = "no"
+        String name = "jebbe"; // Remember equals
+        
+        if(doit) // Condition must be true/false
         {
-            turn(2);
+            turn(6);
             // DO THIS!!!
         }
-        
+        doit = !doit;
     }
        
 }
--- Greenfoot/IfForAndBooleanOperators/MyWorld.java
+++ Greenfoot/IfForAndBooleanOperators/MyWorld.java
@@ -1,21 +1,38 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MyWorld extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        int xSpace = 40;
+        int ySpace = 40;
+        // for(instantiation;condition;increment)
+        for(int x=0;x<600/xSpace;x++)
+        {
+            for(int y=0;y<400/ySpace;y++)
+            {
+                Flower flower = new Flower();
+                //xSpace = xSpace * 0.9;
+                //ySpace = ySpace * 0.9;
+                addObject(flower,x*xSpace+xSpace/2,y*xSpace*xSpace+ySpace/2);
+            }
+        }
+        String n = "Hi ";
+        
+        int d = 0;
+        int stopNumber = 40;
+        
+        int x = 2;
+        int y = 3;
+        
+        float p = (float)Math.PI;
+        showText(""+p, 200, 100);
+        while(stopNumber>0)
+        {
+            Flower flower = new Flower();
+            addObject(flower,200,200);
+            stopNumber = stopNumber - 1;
+        }
     }
 }
