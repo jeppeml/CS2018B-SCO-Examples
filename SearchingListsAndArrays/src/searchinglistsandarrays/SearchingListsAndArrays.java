83d470e589e6dc983eb1ae15cf8e005067eee8cd
jeppeml <jeppeml@users.noreply.github.com>
2018-10-05 09:56:53 +0200

    Fixed binary search

--- SearchingListsAndArrays/src/searchinglistsandarrays/SearchingListsAndArrays.java
+++ SearchingListsAndArrays/src/searchinglistsandarrays/SearchingListsAndArrays.java
@@ -17,11 +17,11 @@ public class SearchingListsAndArrays {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Random rand = new Random();
+        Random rand = new Random(2);
         int size = 100;
         int[] myInts = new int[size];
         for (int i = 0; i < myInts.length-1; i++) {
-            myInts[i] = rand.nextInt(size);
+            myInts[i] = i;//rand.nextInt(size);
             System.out.println(i+": "+myInts[i]);
         }
         myInts[size-1] = size+1;
@@ -29,11 +29,36 @@ public class SearchingListsAndArrays {
         
         int whatImSearchingFor = size+1;
         
-        int index = linearSearch(myInts, whatImSearchingFor);
+        //int index = linearSearch(myInts, whatImSearchingFor);
+        int index = binarySearch(myInts, whatImSearchingFor);
         System.out.println("index: " + index);
         
         
         // TODO code application logic here
+    }
+    
+    public static int binarySearch(int[] arr, int lookingFor)
+    {
+        int count = 0;
+        int first = 0;
+        int last = arr.length-1;
+        int mid = 0;
+        
+        while(first <= last)
+        {
+            count++;
+            mid = (last+first)/2;
+            if(lookingFor == arr[mid])
+            {
+                System.out.println("Count: " + count);
+                return mid;
+            }    
+            else if(lookingFor<arr[mid])
+                last = mid - 1;
+            else // if(lookingFor>arr[mid])
+                first = mid + 1;
+        }
+        return -1;
     }
     
     public static int linearSearch(int[] arr, int lookingFor)
