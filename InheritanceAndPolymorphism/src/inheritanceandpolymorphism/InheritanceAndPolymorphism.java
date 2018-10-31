5820f0b5cb785ef1a454f0147c0c2baab4cab130
jeppeml <jeppeml@users.noreply.github.com>
2018-10-31 11:18:35 +0100

    adding a test class

--- InheritanceAndPolymorphism/src/inheritanceandpolymorphism/InheritanceAndPolymorphism.java
+++ InheritanceAndPolymorphism/src/inheritanceandpolymorphism/InheritanceAndPolymorphism.java
@@ -17,27 +17,9 @@ public class InheritanceAndPolymorphism {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Robot mrRobot = new Robot();
-        mrRobot.setName("Pietr Stegreskey");
-        mrRobot.setBrand("Xiomi");
-        
-        Robot hum = new Humanoid();
-        hum.setName("Piotr Stroganoff");
-        hum.setBrand("HTC");
-        
-        Robot lwn = new Lawnmower();
-        lwn.setName("Peter Cutter");
-        lwn.setBrand("Husquarna");
-        
-        ArrayList<Robot> list = new ArrayList();
-        list.add(lwn);
-        list.add(hum);
-        list.add(mrRobot);
-        for (Robot robot : list) {
-            System.out.println(robot.getName());
-            System.out.println(robot.getBrand());
-            robot.doSomething();
-        }
+        TestIt test = new TestIt();
+        test.run();
+              
     }
     
 }
---
+++ InheritanceAndPolymorphism/src/inheritanceandpolymorphism/TestIt.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritanceandpolymorphism;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jeppjleemoritzled
+ */
+public class TestIt {
+    public void run()
+    {
+        Robot mrRobot = new Robot();
+        mrRobot.setName("Pietr Stegreskey");
+        mrRobot.setBrand("Xiomi");
+        
+        Robot hum = new Humanoid();
+        hum.setName("Piotr Stroganoff");
+        hum.setBrand("HTC");
+        
+        Robot lwn = new Lawnmower();
+        lwn.setName("Peter Cutter");
+        lwn.setBrand("Husquarna");
+        
+        ArrayList<Robot> list = new ArrayList();
+        list.add(lwn);
+        list.add(hum);
+        list.add(mrRobot);
+        for (Robot robot : list) {
+            // Polymorphism to add lastname for instances of subclasses
+            System.out.println(robot.getName());
+            // No polymorphism
+            System.out.println(robot.getBrand());
+            // Polymorphism for acting differently based on type
+            robot.doSomething();
+        }
+    }
+}
