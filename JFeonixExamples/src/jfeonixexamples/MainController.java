17d60a2688bc2ea4b94a9359491c006aaae917a6
jeppeml <jeppeml@users.noreply.github.com>
2019-02-06 10:48:50 +0100

    bindings and stuff

--- JFeonixExamples/src/jfeonixexamples/MainController.java
+++ JFeonixExamples/src/jfeonixexamples/MainController.java
@@ -5,12 +5,18 @@
  */
 package jfeonixexamples;
 
+import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.NumberValidator;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.StringConverter;
 
 /**
  *
@@ -20,6 +26,14 @@ public class MainController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private JFXTextField txtSomeInput;
+    @FXML
+    private JFXSlider slide;
+    @FXML
+    private JFXTextField txtOther;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -29,7 +43,50 @@ public class MainController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        NumberValidator nv = new NumberValidator("This doesn't look like a number?");
+        nv.setMessage("WRONG!");
+        txtSomeInput.setValidators(nv);
+        txtSomeInput.setText("");
+        txtSomeInput.textProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    
+            //nv.validate();
+            try{
+                Double d = Double.parseDouble(newValue);
+            }
+            catch (NumberFormatException nfe)
+            {
+                txtSomeInput.setText(oldValue);
+            }
+        });
+        
+        slide.valueProperty().addListener((observable, oldValue, newValue) -> {
+            txtSomeInput.setText(newValue+"");
+        });
+        
+        //txtOther.textProperty().bind(txtSomeInput.textProperty());
+        txtOther.textProperty().set("df");
+        
+        Bindings.bindBidirectional(
+                txtOther.textProperty(), slide.valueProperty(), new StringConverter<Number>() {
+            @Override
+            public String toString(Number num) {
+                return num.toString();
+            }
+
+            @Override
+            public Number fromString(String string) {
+                try{
+                    Double d = Double.parseDouble(string);
+                    return d;
+                }
+                catch (NumberFormatException nfe)
+                {
+                    return 0;
+                }
+            }
+           
+        });
     }    
     
 }
